9210d08c6bb16101a4319e9d04812239374e562b
jcmartinez <jcmartinezcu@gmail.com>
2019-12-26 20:34:29 +0100

    video 54

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -30,6 +31,10 @@ public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 4327690299451102763L;
 
+	@PrePersist
+	public void prePersit() {
+		createAt = new Date();
+	}
 	public Long getId() {
 		return id;
 	}
