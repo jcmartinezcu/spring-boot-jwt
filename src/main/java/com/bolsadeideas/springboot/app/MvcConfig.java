8e29a97aac1123e891316de2c4ee3554632412a5
juanc <juanc@192.168.1.13>
2020-01-04 19:09:57 +0100

    video 82

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,19 +1,30 @@
 package com.bolsadeideas.springboot.app;
 
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		
+		
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		
+		log.info(resourcePath);
+		
 		registry.addResourceHandler("/uploads/**")
-		.addResourceLocations("file:/D:/Spring5/Temp/uploads/");
+		.addResourceLocations(resourcePath);
 	}
 	
 	
