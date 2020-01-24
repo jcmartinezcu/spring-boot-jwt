5d6b43022bbd0fb845c708afabeecf759db7c5c1
juanc <juanc@192.168.1.13>
2020-01-24 17:58:50 +0100

    video 167

--- src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlxsView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlxsView.java
@@ -5,9 +5,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.bolsadeideas.springboot.app.models.entity.Factura;
 
 @Component("factura/ver.xlsx")
 public class FacturaXlxsView extends AbstractXlsxView{
@@ -15,7 +20,27 @@ public class FacturaXlxsView extends AbstractXlsxView{
 	@Override
 	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+
+		Factura factura = (Factura) model.get("factura");
+		
+		Sheet sheet = workbook.createSheet("Factura spring");
+		
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);		
+		cell.setCellValue("Datos del cliente");	
+		
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido()); 
+		
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getEmail());
+		
+		sheet.createRow(4).createCell(0).setCellValue("Datos de la factura");
+		sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+		sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
+		sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
 		
 	}
 
