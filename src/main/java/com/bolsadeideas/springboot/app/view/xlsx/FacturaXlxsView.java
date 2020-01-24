b5fa2943b200f5f7e61a8f3d43925f8390473003
juanc <juanc@192.168.1.13>
2020-01-24 18:35:43 +0100

    video 169

--- src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlxsView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlxsView.java
@@ -5,7 +5,11 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -22,6 +26,7 @@ public class FacturaXlxsView extends AbstractXlsxView{
 	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
+		response.setHeader("Content-Disposition", "attachment; filename=\"factura_view.xlsx\"");
 		Factura factura = (Factura) model.get("factura");
 		
 		Sheet sheet = workbook.createSheet("Factura spring");
@@ -43,24 +48,61 @@ public class FacturaXlxsView extends AbstractXlsxView{
 		sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
 		sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
 		
+		CellStyle theaderStyle = workbook.createCellStyle();
+		theaderStyle.setBorderBottom(BorderStyle.MEDIUM);
+		theaderStyle.setBorderTop(BorderStyle.MEDIUM);
+		theaderStyle.setBorderRight(BorderStyle.MEDIUM);
+		theaderStyle.setBorderLeft(BorderStyle.MEDIUM);
+		
+		theaderStyle.setFillForegroundColor(IndexedColors.GOLD.index);
+		theaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		
+		
+		CellStyle tbodyStyle = workbook.createCellStyle();
+		tbodyStyle.setBorderBottom(BorderStyle.THIN);
+		tbodyStyle.setBorderTop(BorderStyle.THIN);
+		tbodyStyle.setBorderRight(BorderStyle.THIN);
+		tbodyStyle.setBorderLeft(BorderStyle.THIN);
+		
 		Row header = sheet.createRow(9);
 		header.createCell(0).setCellValue("Producto");
 		header.createCell(1).setCellValue("Precio");
 		header.createCell(2).setCellValue("Cantidad");
 		header.createCell(3).setCellValue("Total");
 		
+		header.getCell(0).setCellStyle(theaderStyle);
+		header.getCell(1).setCellStyle(theaderStyle);
+		header.getCell(2).setCellStyle(theaderStyle);
+		header.getCell(3).setCellStyle(theaderStyle);
+		
 		int rownum = 10;
 		for(ItemFactura item : factura.getItems()) {
 			Row fila = sheet.createRow(rownum++);
-			fila.createCell(0).setCellValue(item.getProducto().getNombre());
-			fila.createCell(1).setCellValue(item.getProducto().getPrecio());
-			fila.createCell(2).setCellValue(item.getCantidad());
-			fila.createCell(3).setCellValue(item.calcularImporte());
+			cell = fila.createCell(0);
+			cell.setCellValue(item.getProducto().getNombre());
+			cell.setCellStyle(tbodyStyle);
+			
+			cell = fila.createCell(1);
+			cell.setCellValue(item.getProducto().getPrecio());
+			cell.setCellStyle(tbodyStyle);
+			
+			cell = fila.createCell(2);
+			cell.setCellValue(item.getCantidad());
+			cell.setCellStyle(tbodyStyle);
+			
+			cell = fila.createCell(3);
+			cell.setCellValue(item.calcularImporte());
+			cell.setCellStyle(tbodyStyle);
 		}
 		
 		Row filaTotal = sheet.createRow(rownum);
-		filaTotal.createCell(2).setCellValue("Gran Total: ");
-		filaTotal.createCell(3).setCellValue(factura.getTotal());
+		cell = filaTotal.createCell(2);
+		cell.setCellValue("Gran Total: ");
+		cell.setCellStyle(tbodyStyle);
+		
+		cell = filaTotal.createCell(3);
+		cell.setCellValue(factura.getTotal());
+		cell.setCellStyle(tbodyStyle);
 		
 		
 		
